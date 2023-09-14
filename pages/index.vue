5bcd72bffd75f15684514aba97056c231ce03dce
ณวพน ลีลานวลิขิต <nawapon@Macintosh.local>
2023-09-14 22:00:02 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
 <template>
     <h1>Index Page</h1>
-    <h2 class="text-red-500 flex items-center justify-center text-4xl">กำลังทำจ้า</h2>
+    <h2 class="text-red-500 flex items-center justify-center text-4xl">กำลังทำจ้า 555</h2>
 </template>
