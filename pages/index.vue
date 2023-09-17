c242682582eb69dfc7d983e840e63dd736abcbc6
ณวพน ลีลานวลิขิต <nawapon.l@ku.th>
2023-09-18 01:08:19 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
             viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                 <path fill="#FA2323" d="M26.4,-37.7C37.3,-28.3,51.5,-24.8,60.8,-15.3C70.1,-5.7,74.5,10,66,17C57.6,24,36.3,22.4,23.2,30.8C10.1,39.2,5,57.6,-7,67.3C-19.1,77,-38.1,77.9,-44.6,67.3C-51.1,56.7,-45.1,34.7,-49.4,17.1C-53.8,-0.6,-68.5,-13.8,-68.7,-25.1C-68.8,-36.3,-54.3,-45.5,-40.4,-53.9C-26.5,-62.4,-13.2,-70.1,-2.8,-66.3C7.7,-62.5,15.4,-47.2,26.4,-37.7Z" transform="translate(100 100)" />
             </svg>
-            <img class="h-3/4 w-96" src="@/assets/images/GoldenTiara2.png" alt="">
+            <img class="h-3/4 w-96 animate-spin" src="@/assets/images/GoldenTiara2.png" alt="">
         </div>
 
         <div class="mt-16 border border-red">
