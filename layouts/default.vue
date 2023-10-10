a494cc67d4e432eae639469ad88fa52494606e25
pingphawat <phawat.po@ku.th>
2023-10-10 16:57:36 +0630

    update goldpawn page and gola

--- layouts/default.vue
+++ layouts/default.vue
@@ -65,6 +65,14 @@
       </div>
     </nav>
   </header>
-
   <slot />
 </template>
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
---
+++ pages/goldpawn/addpawn.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container px-5 py-24 mx-auto">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+        เลขบัตรประชาชนลูกค้า
+      </label>
+      <input
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        id="customer-id"
+        type="text"
+      />
+  
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+        เลขสัญญาตรวจสอบ
+      </label>
+      <input
+        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        id="contract-id"
+        type="text"
+      />
+      <div class="flex justify-end mt-5">
+        <button
+          type="button"
+          class="text-white bg-[#122345] hover:bg-[#122345]/80 focus:ring-4 focus:outline-none focus:ring-[#122345]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#122345]/80 dark:focus:ring-[#122345/40 mr-2 mb-2]"
+        >
+          คำนวณราคาและดอกเบี้ย
+        </button>
+      </div>
+    </div>
+  </template>
+  
