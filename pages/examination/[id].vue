3e99772d2a7626f781ce05c5ab08cf623ac0a45c
kanyakrons <kanyakron.su@gmail.com>
2023-10-28 16:28:51 +0700

    gold image all

--- pages/examination/[id].vue
+++ pages/examination/[id].vue
@@ -39,25 +39,15 @@
         <!-- Gold Card -->
         <div>
           <div class="grid-cols-3 grid gap-20 justify-around mt-20 px-auto  mb-20">
-
             <div v-for="gold of examination.golds" :key="gold.id">
-
               <div class="max-w-xs bg-white border border-gray-200 rounded-lg shadow">
                 <a href="#">
-                  <div class="rounded-t-lg max-h-72 w-72 object-cover">
-                    <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000">
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                      <g id="SVGRepo_iconCarrier">
-                        <path fill="#e1c50e"
-                          d="M435.7 558.7c-.6-3.9-4-6.7-7.9-6.7H166.2c-3.9 0-7.3 2.8-7.9 6.7l-40.2 248c-.1.4-.1.9-.1 1.3 0 4.4 3.6 8 8 8h342c.4 0 .9 0 1.3-.1 4.4-.7 7.3-4.8 6.6-9.2l-40.2-248zM196.5 748l20.7-128h159.5l20.7 128H196.5zm709.4 58.7l-40.2-248c-.6-3.9-4-6.7-7.9-6.7H596.2c-3.9 0-7.3 2.8-7.9 6.7l-40.2 248c-.1.4-.1.9-.1 1.3 0 4.4 3.6 8 8 8h342c.4 0 .9 0 1.3-.1 4.3-.7 7.3-4.8 6.6-9.2zM626.5 748l20.7-128h159.5l20.7 128H626.5zM342 472h342c.4 0 .9 0 1.3-.1 4.4-.7 7.3-4.8 6.6-9.2l-40.2-248c-.6-3.9-4-6.7-7.9-6.7H382.2c-3.9 0-7.3 2.8-7.9 6.7l-40.2 248c-.1.4-.1.9-.1 1.3 0 4.4 3.6 8 8 8zm91.2-196h159.5l20.7 128h-201l20.8-128z">
-                        </path>
-                        <path fill="#E6E6E6"
-                          d="M592.7 276H433.2l-20.8 128h201zM217.2 620l-20.7 128h200.9l-20.7-128zm430 0l-20.7 128h200.9l-20.7-128z">
-                        </path>
-                      </g>
-                    </svg>
-                  </div>
+                    <img v-if="gold.image_path"
+                      class="object-cover w-auto rounded h-60 md:h-[250px] md:w-[320px] md:rounded-none md:rounded-l-lg"
+                      :src="`http://localhost/images/gold/${gold.image_path}`" alt="" />
+                    <img v-else
+                      class="object-cover w-auto rounded h-60 md:h-[250px] md:w-[320px] md:rounded-none md:rounded-l-lg"
+                      src="@/assets/images/gold-default.png" alt="" />
                 </a>
                 <div class="p-5 border-t border-gold">
                   <a href="#">
@@ -103,7 +93,7 @@
         </div>
       </div>
     </div>
-    
+
     <div class="flex justify-center mb-20">
       <button @click="downloadWithCSS"
         class="px-6 py-2 bg-gold text-gray-100 rounded-lg text-xl mx-auto ">ดาวโหลดPDF</button>
@@ -112,8 +102,8 @@
 </template>
 
 <script setup lang="ts">
-  const route = useRoute()
-  const { data: examination } = await useMyFetch<any>(
-    `examination/${route.params.id}`, {}
-  )
+const route = useRoute()
+const { data: examination } = await useMyFetch<any>(
+  `examination/${route.params.id}`, {}
+)
 </script>
