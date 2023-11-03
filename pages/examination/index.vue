5f0881390e33da6dc61acf277cbd2dfce565ac8e
Tonnwp <nameton2107@gmail.com>
2023-11-04 01:05:49 +0700

    examintion sort all

--- pages/examination/index.vue
+++ pages/examination/index.vue
@@ -66,6 +66,55 @@
             <option value="inprogress">inprogress</option>
             <option value="finish">finish</option>
           </select>
+          <div class="flex ml-2 px-4 py-2 bg-darkblue text-white rounded-lg">
+            <button
+              @click="sortExaminationsByDate"
+              class="flex items-center"
+              
+            >
+            <span id="date-sort">กดเพื่อเรียงวันที่</span>
+              
+              <span id="less-to-more" class="flex hidden px-1">
+                น้อยไปมาก
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  class="ml-1 h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M12 3C12.5523 3 13 3.44772 13 4V17.5858L18.2929 12.2929C18.6834 11.9024 19.3166 11.9024 19.7071 12.2929C20.0976 12.6834 20.0976 13.3166 19.7071 13.7071L12.7071 20.7071C12.3166 21.0976 11.6834 21.0976 11.2929 20.7071L4.29289 13.7071C3.90237 13.3166 3.90237 12.6834 4.29289 12.2929C4.68342 11.9024 5.31658 11.9024 5.70711 12.2929L11 17.5858V4C11 3.44772 11.4477 3 12 3Z"
+                      fill="#ffffff"
+                    ></path>
+                  </g>
+                </svg>
+              </span>
+
+              <span id="more-to-less" class="flex hidden px-1">
+                มากไปน้อย
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="ml-1 h-6 w-6"
+                >
+                  <path
+                    d="M12 3C12.2652 3 12.5196 3.10536 12.7071 3.29289L19.7071 10.2929C20.0976 10.6834 20.0976 11.3166 19.7071 11.7071C19.3166 12.0976 18.6834 12.0976 18.2929 11.7071L13 6.41421V20C13 20.5523 12.5523 21 12 21C11.4477 21 11 20.5523 11 20V6.41421L5.70711 11.7071C5.31658 12.0976 4.68342 12.0976 4.29289 11.7071C3.90237 11.3166 3.90237 10.6834 4.29289 10.2929L11.2929 3.29289C11.4804 3.10536 11.7348 3 12 3Z"
+                    fill="#ffffff"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
         </div>
       </div>
 
@@ -232,7 +281,7 @@
             page <= 1,
         }"
       >
-        Prev
+        หน้าก่อน
       </button>
       <button
         @click="page++"
@@ -243,7 +292,7 @@
             page >= Math.ceil(examinations.length / perPage),
         }"
       >
-        Next
+        หน้าถัดไป
       </button>
     </div>
   </section>
@@ -256,6 +305,7 @@ import { useRoute } from "vue-router";
 
 const page = ref(1); // เพิ่ม ref สำหรับ page
 const perPage = ref(10); // เพิ่ม ref สำหรับ perPage
+const sortOrder = ref("less");
 const searchIdDate = ref("");
 const searchIdText = ref("");
 const searchIdText1 = ref("");
@@ -322,4 +372,43 @@ const applyFilter_status = () => {
 
   examinations.value = filteredExaminations;
 };
+
+const sortExaminationsByDate = () => {
+  const moreToLess = document.getElementById("more-to-less") as HTMLElement;
+  const LessToMore = document.getElementById("less-to-more") as HTMLElement;
+  const dateSort = document.getElementById("date-sort") as HTMLElement;
+
+  if (examinations.value) {
+    if (sortOrder.value === "less") {
+      // Sort in ascending order น้อยไปมาก
+      examinations.value.sort(
+        (a, b) => new Date(a.contract_date) - new Date(b.contract_date),
+        moreToLess.classList.add('hidden'),
+        dateSort.classList.add('hidden'),
+        LessToMore.classList.remove("hidden")
+      );
+      sortOrder.value = "more"; // Set sorting order to descending มากไปน้อย
+
+    } else {
+      LessToMore.classList.add("hidden"),
+      moreToLess.classList.remove("hidden")
+      // Sort in descending order มากไปน้อย
+      examinations.value.sort(
+        (a, b) => new Date(b.contract_date) - new Date(a.contract_date),
+        
+      );
+      sortOrder.value = "less"; // Set sorting order to ascending น้อยไปมาก
+    }
+  }
+};
+
+const sortedExaminations = computed(() => {
+  if (Array.isArray(examinations.value)) {
+    const start = (page.value - 1) * perPage.value;
+    const end = start + perPage.value;
+    return examinations.value.slice(start, end);
+  } else {
+    return [];
+  }
+});
 </script>
