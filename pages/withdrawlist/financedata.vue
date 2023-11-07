633be6559066a0b7fa61d95642424bdded204d87
Pariphatt <pariphat.m@ku.th>
2023-11-07 13:19:30 +0700

    update before present

--- pages/withdrawlist/financedata.vue
+++ pages/withdrawlist/financedata.vue
@@ -6,67 +6,167 @@
 
     <div class="mt-10 items-center justify-center md:flex md:flex-wrap">
       <div
-        class="bg-white border border-gold justify-center rounded-lg shadow md:w-5/12 mt-6 mx-auto"
-        v-for="transaction in paginatedTranactions"
-        :key="transaction.id"
+          class="bg-white border border-gold justify-center rounded-lg shadow md:w-5/12 mt-6 mx-auto"
+          v-for="transaction in paginatedTranactions"
+          :key="transaction.id"
       >
-        <div class="p-4 ml-7 leading-normal">
-          <p class="mb-3 font-normal text-gray-700 text-base">
-            รหัส: {{ transaction.id }}
-          </p>
-          <p class="mb-3 font-normal text-gray-700 text-base">
-            เลขบัตรประชาชนลูกค้า: {{ transaction.customer_id }}
-          </p>
-          <p class="mb-3 font-normal text-gray-700 text-base">
-            รหัสพนักงานที่เบิก: {{ transaction.created_by }}
-          </p>
-          <p class="mb-3 font-normal text-gray-700 text-base">
-            จำนวนเงิน: {{ transaction.amount }}
-          </p>
-          <p class="mb-3 font-normal text-gray-700 text-base">
-            สถานะการเบิก: {{ transaction.status }}
-          </p>
-          <p class="mb-3 font-normal text-gray-700 text-base">
-            เลขสัญญาการจำนำ: {{ transaction.pawn_id }}
-          </p>
-          <p class="mb-3 font-normal text-gray-700 text-base">
-            วันเวลาเบิก: {{ transaction.transaction_dateTime }}
-          </p>
-          <div transition="status === 'completed'"><div class="flex mb-4 mt-6">
-            <button
-                @click="confirmAction('completed', transaction)"
-                class="px-10 py-2 bg-green-600 mr-2 border text-white rounded-lg"
-            >
-              ยืนยัน
-            </button>
-            <button
-                @click="confirmAction('rejected', transaction)"
-                class="px-2 py-2 bg-red-600 mr-2 border text-white rounded-lg"
-            >
-              ยกเลิกการเบิกเงิน
-            </button>
-          </div></div>
+        <div v-if="transaction.status === 'inprogress'">
+          <div class="p-4 ml-7 leading-normal">
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              รหัส: {{ transaction.id }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              เลขบัตรประชาชนลูกค้า: {{ transaction.customer_id }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              รหัสพนักงานที่เบิก: {{ transaction.created_by }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              จำนวนเงิน: {{ transaction.amount }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              สถานะการเบิก:
+              <span
+                  v-if="transaction.status === 'completed'"
+                  class="text-green-500"
+              >
+                {{ transaction.status }}
+              </span>
 
-          <div
-            v-if="showConfirmationModal"
-            class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-          >
-            <div class="bg-white p-8 rounded-lg">
-              <h2 class="text-xl font-bold mb-4">ยืนยันการดำเนินการ</h2>
-              <p>คุณแน่ใจที่ต้องการดำเนินการนี้หรือไม่?</p>
-              <div class="mt-4 flex justify-end">
-                <button
-                  @click="updateStatus"
-                  class="px-4 py-2 bg-green-600 mr-2 border text-white rounded-lg"
-                >
-                  ยืนยัน
-                </button>
-                <button
-                  @click="cancelAction"
-                  class="px-4 py-2 bg-red-600 text-white rounded-lg"
-                >
-                  ยกเลิก
-                </button>
+              <span
+                  v-if="transaction.status === 'rejected'"
+                  class="text-red-500"
+              >
+              </span>
+              <span v-else class="text-blue-500">
+                {{ transaction.status }}
+              </span>
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              เลขสัญญาการจำนำ: {{ transaction.pawn_id }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              วันเวลาเบิก:
+              {{
+                new Date(transaction.transaction_dateTime).toLocaleDateString(
+                    "th-TH",
+                    {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                    }
+                )
+              }}
+            </p>
+
+            <div class="flex mb-4 mt-6">
+              <button
+                  @click="confirmAction('completed', transaction)"
+                  class="px-10 py-2 bg-green-600 mr-2 border text-white rounded-lg"
+              >
+                ยืนยัน
+              </button>
+              <button
+                  @click="confirmAction('rejected', transaction)"
+                  class="px-2 py-2 bg-red-600 mr-2 border text-white rounded-lg"
+              >
+                ยกเลิกการเบิกเงิน
+              </button>
+            </div>
+
+            <div
+                v-if="showConfirmationModal"
+                class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+            >
+              <div class="bg-white p-8 rounded-lg">
+                <h2 class="text-xl font-bold mb-4">ยืนยันการดำเนินการ</h2>
+                <p>คุณแน่ใจที่ต้องการดำเนินการนี้หรือไม่?</p>
+                <div class="mt-4 flex justify-end">
+                  <button
+                      @click="updateStatus"
+                      class="px-4 py-2 bg-green-600 mr-2 border text-white rounded-lg"
+                  >
+                    ยืนยัน
+                  </button>
+                  <button
+                      @click="cancelAction"
+                      class="px-4 py-2 bg-red-600 text-white rounded-lg"
+                  >
+                    ยกเลิก
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div v-else>
+          <div class="px-4 py-12 ml-7 leading-normal">
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              รหัส: {{ transaction.id }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              เลขบัตรประชาชนลูกค้า: {{ transaction.customer_id }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              รหัสพนักงานที่เบิก: {{ transaction.created_by }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              จำนวนเงิน: {{ transaction.amount }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              สถานะการเบิก:
+              <span
+                  v-if="transaction.status === 'completed'"
+                  class="text-green-500"
+              >
+                {{ transaction.status }}
+              </span>
+              <span
+                  v-if="transaction.status === 'rejected'"
+                  class="text-red-500"
+              >
+                {{ transaction.status }}
+              </span>
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              เลขสัญญาการจำนำ: {{ transaction.pawn_id }}
+            </p>
+            <p class="mb-3 font-normal text-gray-700 text-base">
+              วันเวลาเบิก:
+              {{
+                new Date(transaction.transaction_dateTime).toLocaleDateString(
+                    "th-TH",
+                    {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                    }
+                )
+              }}
+            </p>
+
+            <div
+                v-if="showConfirmationModal"
+                class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+            >
+              <div class="bg-white p-8 rounded-lg">
+                <h2 class="text-xl font-bold mb-4">ยืนยันการดำเนินการ</h2>
+                <p>คุณแน่ใจที่ต้องการดำเนินการนี้หรือไม่?</p>
+                <div class="mt-4 flex justify-end">
+                  <button
+                      @click="updateStatus"
+                      class="px-4 py-2 bg-green-600 mr-2 border text-white rounded-lg"
+                  >
+                    ยืนยัน
+                  </button>
+                  <button
+                      @click="cancelAction"
+                      class="px-4 py-2 bg-red-600 text-white rounded-lg"
+                  >
+                    ยกเลิก
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -75,10 +175,10 @@
     </div>
     <div class="flex items-center justify-center mb-14 mt-6">
       <button
-        @click="page--"
-        :disabled="page <= 1"
-        class="mr-2 text-lg bg-darkblue hover:bg-gradient-to-b from-gold to-darkgold focus:ring-2 focus:ring-gold focus:outline-none rounded-lg text-white px-8 py-2"
-        :class="{
+          @click="page--"
+          :disabled="page <= 1"
+          class="mr-2 text-lg bg-darkblue hover:bg-gradient-to-b from-gold to-darkgold focus:ring-2 focus:ring-gold focus:outline-none rounded-lg text-white px-8 py-2"
+          :class="{
           'disabled:bg-gold disabled:text-white disabled:cursor-not-allowed':
             page <= 1,
         }"
@@ -87,10 +187,10 @@
       </button>
 
       <button
-        @click="page++"
-        :disabled="page >= Math.ceil(transactions.length / perPage)"
-        class="mr-2 text-lg bg-darkblue hover:bg-gradient-to-b from-gold to-darkgold focus:ring-2 focus:ring-gold focus:outline-none rounded-lg text-white px-8 py-2"
-        :class="{
+          @click="page++"
+          :disabled="page >= Math.ceil(transactions.length / perPage)"
+          class="mr-2 text-lg bg-darkblue hover:bg-gradient-to-b from-gold to-darkgold focus:ring-2 focus:ring-gold focus:outline-none rounded-lg text-white px-8 py-2"
+          :class="{
           'disabled:bg-gold  disabled:text-white disabled:cursor-not-allowed':
             page >= Math.ceil(transactions.length / perPage),
         }"
@@ -154,8 +254,8 @@ const updateStatus = async () => {
       window.location.reload();
 
       const { data } = await useMyFetch<any>(
-        `transaction/${transactionId}`,
-        {}
+          `transaction/${transactionId}`,
+          {}
       );
       selectedTransaction.value = data;
       showConfirmationModal.value = false;
